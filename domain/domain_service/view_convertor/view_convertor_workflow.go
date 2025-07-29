65a54c037de5e116e2d2b454f50eafefb79e79b7
mr <morgane.roques@irt-saintexupery.com>
2025-07-29 14:41:08 +0200

    test

--- domain/domain_service/view_convertor/view_convertor_workflow.go
+++ domain/domain_service/view_convertor/view_convertor_workflow.go
@@ -42,7 +42,7 @@ func (d *ViewConvertor) InitializeWorkflow(record map[string]interface{}) sm.Wor
 		IsDismiss: record["state"] == "dismiss" || record["state"] == "refused" || record["state"] == "canceled",
 		Current:   utils.ToString(record["current_index"]),
 		Position:  utils.ToString(record["current_index"]),
-		IsClose:   utils.GetBool(record, "is_close") || record["state"] == "dismiss" || record["state"] == "refused" || record["state"] == "canceled",
+		IsClose:   utils.GetBool(record, "is_close") || record["state"] == "dismiss" || record["state"] == "refused" || record["state"] == "completed" || record["state"] == "canceled",
 	}
 }
 
