41a812287b8e6223806b45946942cbff938d8065
mr <morgane.roques@irt-saintexupery.com>
2025-10-17 17:08:04 +0200

    verify false

--- infrastructure/service/row.go
+++ infrastructure/service/row.go
@@ -121,7 +121,7 @@ func (t *TableRowService) Update(record map[string]interface{}, restriction ...s
 }
 func (t *TableRowService) Delete(restriction ...string) ([]map[string]interface{}, error) {
 	var err error
-	if _, err = t.setupFilter(map[string]interface{}{}, true, true, restriction...); err != nil {
+	if _, err = t.setupFilter(map[string]interface{}{}, false, true, restriction...); err != nil {
 		return nil, err
 	}
 	if t.Results, err = t.Get(restriction...); err == nil {
