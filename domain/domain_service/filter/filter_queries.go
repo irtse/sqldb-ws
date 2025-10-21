c464f7ac6264f86dcce7240415a9bd656202c1e8
mr <morgane.roques@irt-saintexupery.com>
2025-10-21 17:33:57 +0200

    test

--- domain/domain_service/filter/filter_queries.go
+++ domain/domain_service/filter/filter_queries.go
@@ -62,7 +62,8 @@ func (s *FilterService) getFilterReadonly(schema sm.SchemaModel, isUpdate bool) 
 			"!is_close": true,
 		}, false)+")")
 		subrestr = append(subrestr, "("+connector.FormatSQLRestrictionWhereByMap("", map[string]interface{}{
-			ds.UserDBField: s.Domain.GetUserID(),
+			"binded_dbtask": "main.id",
+			ds.UserDBField:  s.Domain.GetUserID(),
 			ds.UserDBField + "_1": s.Domain.GetDb().BuildSelectQueryWithRestriction(ds.DBDelegation.Name, map[string]interface{}{
 				ds.UserDBField: s.Domain.GetUserID(),
 			}, false, "delegated_"+ds.UserDBField),
