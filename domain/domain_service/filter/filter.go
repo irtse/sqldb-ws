3b0242c1ca1c29b4d8c37ac619b307d941d2e97e
mr <morgane.roques@irt-saintexupery.com>
2025-10-24 14:36:28 +0200

    test

--- domain/domain_service/filter/filter.go
+++ domain/domain_service/filter/filter.go
@@ -84,9 +84,9 @@ func (f *FilterService) GetQueryFilter(tableName string, domainParams utils.Para
 		if f.Domain.GetMethod() != utils.DELETE && !avoidUser && !schema.IsAssociated {
 			SQLrestriction = f.RestrictionByEntityUser(schema, SQLrestriction, false) // admin can see all on admin view
 		}
-		SQLrestriction = f.GetFilterEdit(SQLrestriction, schema)
-		SQLrestriction = f.GetFilterDelete(SQLrestriction, schema)
 	}
+	SQLrestriction = f.GetFilterEdit(SQLrestriction, schema)
+	SQLrestriction = f.GetFilterDelete(SQLrestriction, schema)
 	return strings.Join(SQLrestriction, " AND "), strings.Join(SQLOrder, ","), SQLLimit, strings.Join(SQLview, ",")
 }
 
