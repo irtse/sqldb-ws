dfcdf858e345b8a375de29c60a7576169e73cfff
mr <morgane.roques@irt-saintexupery.com>
2025-10-28 08:23:01 +0100

    prod

--- cegid-demo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd plugins/cegid
-go build -buildmode=plugin -o plugin.so plugin.go
-
-cd ../autoload_cegid
-go build -buildmode=plugin -o plugin.so plugin.go
-
-cd ../..
-go run main.go
