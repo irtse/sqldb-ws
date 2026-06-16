62ff69d8ba20e8df0aa3edbbb2fc3dad1b1a9718
mr <morgane.roques@irt-saintexupery.com>
2026-06-16 08:02:51 +0200

    test

--- script.sh
+++ script.sh
@@ -1,13 +1,19 @@
+#!/bin/bash
+
+INPUT="publication_test4.csv"
+OUTPUT="publication_test.csv"
+
 awk -F';' '
 NR==1 {
     for (i=1; i<=NF; i++) {
-        if ($i == "p_actif") {
-            col = i
+        if ($i == "\"p_actif\"") {
+            col=i
             break
         }
     }
     print
     next
 }
-$col == 1
-' input.csv > output.csv
+
+$col == "\"1\""
+' "$INPUT" > "$OUTPUT"
