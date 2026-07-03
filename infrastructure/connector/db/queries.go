05dd35951a649cb06c780b5be5302dccdffad7a2
mr <morgane.roques@irt-saintexupery.com>
2026-07-03 11:37:58 +0200

    query

--- infrastructure/connector/db/queries.go
+++ infrastructure/connector/db/queries.go
@@ -183,8 +183,11 @@ func (db *Database) UpdateQuery(name string, record map[string]interface{}, rest
 	if err != nil {
 		return err
 	}
-	fmt.Println(q)
+
 	err = db.Query(q)
+	if err != nil {
+		fmt.Println(q)
+	}
 	if err != nil && strings.Contains(err.Error(), "unique") {
 		splitted := strings.Split(err.Error(), "\"")
 		if len(splitted) > 1 {
