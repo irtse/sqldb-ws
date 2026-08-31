2d880f461517c3f3246ff0018964a23230634ec5
mr <morgane.roques@irt-saintexupery.com>
2026-08-31 12:38:28 +0200

    set up

--- domain/domain_service/view_convertor/view_convertor.go
+++ domain/domain_service/view_convertor/view_convertor.go
@@ -175,12 +175,10 @@ func (v *ViewConvertor) transformShallowedView(results utils.Results, tableName 
 	scheme, id, order, _, addAction, _ := v.GetViewFields(t, false, utils.Results{})
 	for _, record := range results {
 		if _, ok := record["is_draft"]; ok && record.GetBool("is_draft") && !v.Domain.IsOwn(false, false, utils.SELECT) {
-			fmt.Println("SKIP", ok, record.GetBool("is_draft"), !v.Domain.IsOwn(false, false, utils.SELECT))
 			continue
 		}
 		if record.GetString(sm.NAMEKEY) == "" {
 			res = append(res, record)
-			fmt.Println("SKIP2")
 			continue
 		}
 		newView := v.createShallowedViewItem(record, tableName, &sch, max, isWorkflow)
--- domain/specialized_service/task_service/workflow_service.go
+++ domain/specialized_service/task_service/workflow_service.go
@@ -47,7 +47,7 @@ func (s *WorkflowService) TransformToGenericView(results utils.Results, tableNam
 func (s *WorkflowService) GenerateQueryFilter(tableName string, innerestr ...string) (string, string, string, string) {
 	s1, s2, s3, s4 := filter.NewFilterService(s.Domain).GetQueryFilter(tableName, s.Domain.GetParams().Copy(), false, innerestr...)
 	fmt.Println("WORKFLOW QUERY", s1, s2, s3, s4, innerestr)
-	return s1, s2, s3, s4
+	return s1, s2, "", s4
 }
 
 func (s *WorkflowService) VerifyDataIntegrity(record map[string]interface{}, tablename string) (map[string]interface{}, error, bool) {
